07fe0e6b507f8c5f033969272cba1922c4a7b5ed
Sumit-Sharma00 <142771342+Sumit-Sharma00@users.noreply.github.com>
2024-08-04 13:59:47 +0530

    Add files via upload

---
+++ hackathon/javascript.js
@@ -0,0 +1,113 @@
+function showAbout(){
+    document.getElementById("aboutDetails").style.display="block";
+    document.getElementById("importnaceDetails").style.display="none";
+    document.getElementById("pubertyDetails").style.display="none";
+    document.getElementById("sexualHealthDetails").style.display="none";
+    document.getElementById("faqDetails").style.display="none";
+    document.getElementById("resourcesDetails").style.display="none";
+}
+
+function showImportance(){
+    document.getElementById("aboutDetails").style.display="none";
+    document.getElementById("importnaceDetails").style.display="block";
+    document.getElementById("pubertyDetails").style.display="none";
+    document.getElementById("sexualHealthDetails").style.display="none";
+    document.getElementById("faqDetails").style.display="none";
+    document.getElementById("resourcesDetails").style.display="none";
+}
+
+function showPuberty(){
+    document.getElementById("aboutDetails").style.display="none";
+    document.getElementById("importnaceDetails").style.display="none";
+    document.getElementById("pubertyDetails").style.display="block";
+    document.getElementById("sexualHealthDetails").style.display="none";
+    document.getElementById("faqDetails").style.display="none";
+    document.getElementById("resourcesDetails").style.display="none";
+}
+
+function showSexualHealth(){
+    document.getElementById("aboutDetails").style.display="none";
+    document.getElementById("importnaceDetails").style.display="none";
+    document.getElementById("pubertyDetails").style.display="none";
+    document.getElementById("sexualHealthDetails").style.display="block";
+    document.getElementById("faqDetails").style.display="none";
+    document.getElementById("resourcesDetails").style.display="none";
+}
+
+function showFAQ(){
+    document.getElementById("aboutDetails").style.display="none";
+    document.getElementById("importnaceDetails").style.display="none";
+    document.getElementById("pubertyDetails").style.display="none";
+    document.getElementById("sexualHealthDetails").style.display="none";
+    document.getElementById("faqDetails").style.display="block";
+    document.getElementById("resourcesDetails").style.display="none";
+}
+
+function showResources(){
+    document.getElementById("aboutDetails").style.display="none";
+    document.getElementById("importnaceDetails").style.display="none";
+    document.getElementById("pubertyDetails").style.display="none";
+    document.getElementById("sexualHealthDetails").style.display="none";
+    document.getElementById("faqDetails").style.display="none";
+    document.getElementById("resourcesDetails").style.display="block";
+}
+
+
+
+
+
+
+
+function showAboutFemale(){
+    document.getElementById("aboutDetailsFemale").style.display="block";
+    document.getElementById("importnaceDetailsFemale").style.display="none";
+    document.getElementById("pubertyDetailsFemale").style.display="none";
+    document.getElementById("sexualHealthDetailsFemale").style.display="none";
+    document.getElementById("faqDetailsFemale").style.display="none";
+    document.getElementById("resourcesDetailsFemale").style.display="none";
+}
+
+function showImportanceFemale(){
+    document.getElementById("aboutDetailsFemale").style.display="none";
+    document.getElementById("importnaceDetailsFemale").style.display="block";
+    document.getElementById("pubertyDetailsFemale").style.display="none";
+    document.getElementById("sexualHealthDetailsFemale").style.display="none";
+    document.getElementById("faqDetailsFemale").style.display="none";
+    document.getElementById("resourcesDetailsFemale").style.display="none";
+}
+
+function showPubertyFemale(){
+    document.getElementById("aboutDetailsFemale").style.display="none";
+    document.getElementById("importnaceDetailsFemale").style.display="none";
+    document.getElementById("pubertyDetailsFemale").style.display="block";
+    document.getElementById("sexualHealthDetailsFemale").style.display="none";
+    document.getElementById("faqDetailsFemale").style.display="none";
+    document.getElementById("resourcesDetailsFemale").style.display="none";
+}
+
+function showSexualHealthFemale(){
+    document.getElementById("aboutDetailsFemale").style.display="none";
+    document.getElementById("importnaceDetailsFemale").style.display="none";
+    document.getElementById("pubertyDetailsFemale").style.display="none";
+    document.getElementById("sexualHealthDetailsFemale").style.display="block";
+    document.getElementById("faqDetailsFemale").style.display="none";
+    document.getElementById("resourcesDetailsFemale").style.display="none";
+}
+
+function showFAQFemale(){
+    document.getElementById("aboutDetailsFemale").style.display="none";
+    document.getElementById("importnaceDetailsFemale").style.display="none";
+    document.getElementById("pubertyDetailsFemale").style.display="none";
+    document.getElementById("sexualHealthDetailsFemale").style.display="none";
+    document.getElementById("faqDetailsFemale").style.display="block";
+    document.getElementById("resourcesDetailsFemale").style.display="none";
+}
+
+function showResourcesFemale(){
+    document.getElementById("aboutDetailsFemale").style.display="none";
+    document.getElementById("importnaceDetailsFemale").style.display="none";
+    document.getElementById("pubertyDetailsFemale").style.display="none";
+    document.getElementById("sexualHealthDetailsFemale").style.display="none";
+    document.getElementById("faqDetailsFemale").style.display="none";
+    document.getElementById("resourcesDetailsFemale").style.display="block";
+}
